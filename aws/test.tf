116738335fd905f5f4af6df5d59b61339e05e051
Andy Cobaugh <andrew.cobaugh@gmail.com>
2018-03-07 22:50:39 -0500

    add ssh key

--- aws/test.tf
+++ aws/test.tf
@@ -21,4 +21,7 @@ module "k8s" {
   config_s3_bucket = "${aws_s3_bucket.k8s-dev-config.id}"
 
   route53_zone_id = "${var.route53_zone_id}"
+
+	ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC42lZedNWOZF2EXouJVzZ4kolnsW/9gi10KZITWGnw9f32Pq7Z7GjPLoy21FdLZhyROjSom4CoP6/fN408TzDHqtAkmBCsFPsg2ozUBPcdm3tDOT10M+F5KphGUDn1zGCwKR9px57Y6Us/XGxC3Iy2QCVKQomeE96MCXpmUZDRMrvUC9mJyvKbP2DUTADhSqsgnY6hCLMwT74u7pNf6IYL6/bDxK/6dICvZFL3LGv3ckmS4AcT8Xq6uGgMSyVVdYE/JBRcQF5/bsXBUZl5jeGeGjvCN3VYCexbXESGuaS3VIRzuPUuJivxFtBLZVKyjBImT5vz+oN9DlDwBwa46Ul5 phalenor@zirzla"
+
 }
