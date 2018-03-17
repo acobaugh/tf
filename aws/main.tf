bb9437f17b4ac1848fc648d5ec29357d0554e49f
Andy Cobaugh <andrew.cobaugh@gmail.com>
2018-03-17 13:38:02 -0400

    enable private networks and nat gateway

--- aws/main.tf
+++ aws/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source = "../../terraform-aws-vpc"
 
   name = "dev"
   cidr = "10.0.0.0/16"
@@ -7,8 +7,11 @@ module "vpc" {
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.128.0/24", "10.0.129.0/24", "10.0.130.0/24"]
   public_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  
+	enable_nat_gateway = true
+  single_nat_gateway = true
 
-  enable_dns_hostnames = true
+	enable_dns_hostnames = true
 
   tags = {
     Terraform   = "true"
