8b05815ffbfb012d4c915c96d8f01af5b67aa840
Andy Cobaugh <andrew.cobaugh@gmail.com>
2018-03-22 22:18:31 -0400

    terraform fmt

--- aws/main.tf
+++ aws/main.tf
@@ -7,11 +7,11 @@ module "vpc" {
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.128.0/24", "10.0.129.0/24", "10.0.130.0/24"]
   public_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  
-	enable_nat_gateway = true
+
+  enable_nat_gateway = true
   single_nat_gateway = true
 
-	enable_dns_hostnames = true
+  enable_dns_hostnames = true
 
   tags = {
     Terraform   = "true"
