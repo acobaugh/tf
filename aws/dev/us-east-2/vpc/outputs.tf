e64c1f9830f30e84811dcd6f33704db112c557ad
Andy Cobaugh <andrew.cobaugh@gmail.com>
2018-06-20 22:37:04 -0400

    aws/dev/us-east-2/vpc: add outputs

---
+++ aws/dev/us-east-2/vpc/outputs.tf
@@ -0,0 +1,35 @@
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+
+output "vpc_cidr_block" {
+  value = "${module.vpc.vpc_cidr_block}"
+}
+
+output "vpc_ipv6_cidr_block" {
+  value = "${module.vpc.vpc_ipv6_cidr_block}"
+}
+
+output "private_subnets" {
+  value = ["${module.vpc.private_subnets}"]
+}
+
+output "private_subnets_cidr_blocks" {
+  value = ["${module.vpc.private_subnets_cidr_blocks}"]
+}
+
+output "private_subnets_ipv6_cidr_blocks" {
+  value = ["${module.vpc.private_subnets_ipv6_cidr_blocks}"]
+}
+
+output "public_subnets" {
+  value = ["${module.vpc.public_subnets}"]
+}
+
+output "public_subnets_cidr_blocks" {
+  value = ["${module.vpc.public_subnets_cidr_blocks}"]
+}
+
+output "public_subnets_ipv6_cidr_blocks" {
+  value = ["${module.vpc.public_subnets_ipv6_cidr_blocks}"]
+}
