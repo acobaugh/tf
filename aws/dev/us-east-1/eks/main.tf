bb39aa588dbc76252997518575e729f908600f1b
Andy Cobaugh <andrew.cobaugh@gmail.com>
2018-10-24 23:19:26 -0400

    dev: eks instance in us-east-1, remove older k8s instance

---
+++ aws/dev/us-east-1/eks/main.tf
@@ -0,0 +1,42 @@
+module "vpc" {
+  source = "git::https://github.com/acobaugh/terraform-aws-vpc.git?ref=feature/ipv6"
+
+  name = "k8s-dev"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets = ["10.1.128.0/24", "10.1.129.0/24", "10.1.130.0/24"]
+  public_subnets  = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = "test"
+  subnets      = "${module.vpc.public_subnets}"
+
+  tags = {
+    Environment = "dev"
+
+    Terraform = true
+  }
+
+  vpc_id = "${module.vpc.vpc_id}"
+}
+
+output "kubeconfig" {
+  value = "${module.eks.kubeconfig}"
+}
+
+output "cluster_id" {
+  value = "${module.eks.cluster_id}"
+}
---
+++ aws/dev/us-east-1/terraform.tfvars
@@ -0,0 +1,25 @@
+terragrunt = {
+  remote_state {
+    backend = "s3"
+
+    config {
+      bucket  = "acobaugh-tfstate-dev-us-east-1"
+      region  = "us-east-1"
+      key     = "${path_relative_to_include()}/terraform.tfstate"
+      encrypt = true
+    }
+  }
+
+  terraform {
+    extra_arguments "common_vars" {
+      commands = ["init","${get_terraform_commands_that_need_vars()}"]
+
+      optional_var_files = [
+        "${get_parent_tfvars_dir()}/${path_relative_from_include()}/account.tfvars",
+        "${get_parent_tfvars_dir()}/${path_relative_from_include()}/region.tfvars",
+        "${get_tfvars_dir()}/${path_relative_from_include()}/account.tfvars",
+        "${get_tfvars_dir()}/${path_relative_from_include()}/region.tfvars",
+      ]
+    }
+  }
+}
